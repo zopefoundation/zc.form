265dbc04f5ba784d3b2a0e9498d7ac2673a8b03d
Aaron Lehmann <aaron@zope.com>
2007-07-24 19:44:46 +0000

    Make USTimeSource non-contextual, and bump egg version.

--- src/zc/form/source.py
+++ src/zc/form/source.py
@@ -1,14 +1,14 @@
 import pytz
-import zc.sourcefactory.contextual
+import zc.sourcefactory.basic
 
 class USTimeZones(
-    zc.sourcefactory.contextual.BasicContextualSourceFactory):
+    zc.sourcefactory.basic.BasicSourceFactory):
 
     """List of timezones taken from pytz"""
 
     tzs = [tz for tz in pytz.common_timezones if 'US/' in tz]
 
-    def getValues(self, context):
+    def getValues(self):
         return self.tzs
 
     def getTitle(self, value):
