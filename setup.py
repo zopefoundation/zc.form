e9fd2f13a98ab5318f1498a3a7673819ec5ca16b
Michael Howitz <mh@gocept.com>
2016-01-12 09:47:28 +0000

    Preparing release 0.4

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(filepath):
 
 setup(
     name="zc.form",
-    version='0.4.dev0',
+    version='0.4',
     packages=find_packages('src'),
     package_dir={'':'src'},
     namespace_packages=['zc'],
