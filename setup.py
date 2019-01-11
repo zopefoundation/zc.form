db1f082c38798fff8fc51fa317d4c0d03037da8b
Michael Howitz <mh@gocept.com>
2019-01-11 11:53:56 +0100

    Back to development: 1.1

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ TEST_REQUIRES = [
 
 setup(
     name="zc.form",
-    version='1.0',
+    version='1.1.dev0',
     packages=find_packages('src'),
     package_dir={'': 'src'},
     namespace_packages=['zc'],
